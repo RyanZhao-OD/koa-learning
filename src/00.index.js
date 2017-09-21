8f08ba18bf0ec307b97d5cb7814b44525d779f74
zhaorong01 <zhaorong01@baidu.com>
2017-09-21 17:00:20 +0800

    commit

---
+++ src/00.index.js
@@ -0,0 +1,38 @@
+/**
+ * @file
+ * @author zhaorong (zhaorong01@baidu.com)
+ * @date 2017/9/21
+ */
+
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+    ctx.body = 'hello koa2';
+    console.log(11);
+});
+
+router.get('/tologin', async (ctx, next) => {
+    ctx.body = {
+        errorno: 0,
+        data: {
+            a: 1,
+            b: 2
+        }
+    };
+});
+
+// curl -X POST -H 'Cookie: BDUSS=aaa' -H 'X-ik-token: xxx'  http://localhost:8090/login
+router.post('/login', async (ctx, next) => {
+    console.log('------');
+    ctx.body = 'login page';
+});
+
+
+
+app.use(router.routes())
+    .use(router.allowedMethods())
+    .listen(8090, 'localhost');
